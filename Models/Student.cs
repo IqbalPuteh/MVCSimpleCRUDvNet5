a66053f9f3f5b5c2b5d316a719a9a83eea78ca98
IqbalPuteh <Iqbalrp75@gmail.com>
2022-06-22 18:59:33 +0800

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace waDVTechTest.Models
+{
+    public partial class Student
+    {
+        public string NomerId { get; set; }
+        public string Nama { get; set; }
+    }
+}
